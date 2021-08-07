a4046a20e26b1871f3462b3e82ea815786787f43
Mark Yen <77414433+markcyen@users.noreply.github.com>
2021-08-07 12:41:04 -0600

    Implement daily weather method

--- app/poros/weather_data.rb
+++ app/poros/weather_data.rb
@@ -1,8 +1,9 @@
 class WeatherData
-  attr_reader :current_weather
+  attr_reader :current_weather, :daily_weather
 
   def initialize(weather_data)
     @current_weather = current(weather_data)
+    @daily_weather = daily(weather_data)
   end
 
   def current(weather_data)
@@ -19,5 +20,19 @@ class WeatherData
       conditions: current_data[:weather].first[:description],
       icon: current_data[:weather].first[:icon]
     }
+  end
+
+  def daily(weather_data)
+    weather_data[:daily].map do |daily|
+      {
+        date: Time.at(daily[:dt]).strftime('%Y-%m-%d'),
+        sunrise: Time.at(daily[:sunrise]),
+        sunset: Time.at(daily[:sunset]),
+        max_temp: daily[:temp][:max],
+        min_temp: daily[:temp][:min],
+        conditions: daily[:weather].first[:description],
+        icon: daily[:weather].first[:icon]
+      }
+    end[0..4]
   end
 end
