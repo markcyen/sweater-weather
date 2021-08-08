844fe5c38072cd35f7e64400aafb7d2186bc3e33
Mark Yen <77414433+markcyen@users.noreply.github.com>
2021-08-08 10:10:16 -0600

    Create test for unsplash poros

--- spec/poros/unsplash_spec.rb
+++ spec/poros/unsplash_spec.rb
@@ -5,22 +5,25 @@ RSpec.describe 'Unsplash Data Poros' do
     before :all do
       unsplash_file = File.read('spec/fixtures/webmock/unsplash_photo_data.json')
       @denver_photo_data = JSON.parse(unsplash_file, symbolize_names: true)
-      @denver_photo = UnsplashData.new(@denver_photo_data)
+      @denver = UnsplashData.new(@denver_photo_data, 'denver, co')
     end
 
     it 'abstracts and encapsulates photo data that can be read' do
       actual = {
-        location: 'denver, co',
-        image_url: 'https://images.unsplash.com/photo-1619856699906-09e1f58c98b1?crop=entropy&cs=srgb&fm=jpg&ixid=MnwyNTIxNzR8MHwxfHNlYXJjaHwxfHxkZW52ZXIlMkNjb3xlbnwwfHx8fDE2MjgzOTgyMDI&ixlib=rb-1.2.1&q=85',
-        credit: {
-          source: 'https://unsplash.com/',
-          logo: 'https://unsplash.com/s/photos/unsplash-logo',
-          author: 'Ryan De Hamer',
-          author_portfolio: 'http://www.dehamermedia.com',
+        image: {
+          location: 'denver, co',
+          image_url: 'https://images.unsplash.com/photo-1619856699906-09e1f58c98b1?crop=entropy&cs=srgb&fm=jpg&ixid=MnwyNTIxNzR8MHwxfHNlYXJjaHwxfHxkZW52ZXIlMkNjb3xlbnwwfHx8fDE2Mjg0MzcwOTE&ixlib=rb-1.2.1&q=85',
+          credit: {
+            source: 'https://unsplash.com/',
+            logo: 'https://unsplash.com/s/photos/unsplash-logo',
+            author: 'Ryan De Hamer',
+            author_portfolio: 'http://www.dehamermedia.com',
+            unsplash_source_comment: 'Require the image URLs returned by the API to be directly used or embedded in application (also known as hotlinking)'
+          }
         }
       }
 
-      expect(@denver_photo).to eq(actual)
+      expect(@denver.photos_data.first).to eq(actual)
     end
   end
 end
