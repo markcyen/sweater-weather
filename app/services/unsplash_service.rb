95102058625d30f6993abea1a499a547aca02e10
Mark Yen <77414433+markcyen@users.noreply.github.com>
2021-08-08 09:07:00 -0600

    Implement unsplash service class methods

---
+++ app/services/unsplash_service.rb
@@ -0,0 +1,18 @@
+class UnsplashService
+  class << self
+    def retrieve_background(location, per_page = 1)
+      response = conn.get('/search/photos') do |req|
+        req.params['client_id'] = ENV['unsplash_key']
+        req.params['query'] = location
+        req.params['per_page'] = per_page
+      end
+      JSON.parse(response.body, symbolize_names: true)
+    end
+
+    private
+
+    def conn
+      Faraday.new(url: 'https://api.unsplash.com')
+    end
+  end
+end
