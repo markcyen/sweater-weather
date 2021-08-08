d88b8717d527d95efa8b3cf06a83a80dc1f9fb97
Mark Yen <77414433+markcyen@users.noreply.github.com>
2021-08-08 16:19:23 -0600

    Refactor per page param

--- app/facades/image_facade.rb
+++ app/facades/image_facade.rb
@@ -1,5 +1,5 @@
 class ImageFacade
-  def self.get_image_data(search_location, per_page = 1)
+  def self.get_image_data(search_location, per_page)
     image_data = UnsplashService.retrieve_background_image(search_location, per_page)
     UnsplashData.new(image_data, search_location)
   end
