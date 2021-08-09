89755a82adafb907011dfd7388c8c98601667738
Mark Yen <77414433+markcyen@users.noreply.github.com>
2021-08-09 09:59:55 -0600

    Refactor class method to receive city_state param

--- app/services/breweries_service.rb
+++ app/services/breweries_service.rb
@@ -1,6 +1,10 @@
 class BreweriesService
   class << self
-    def retrieve_breweries(distance, quantity)
+    def retrieve_breweries(city_state, quantity)
+      lat_long = GeocodeService.retrieve_geocode(city_state)
+      lat = lat_long[:results].first[:locations].first[:latLng][:lat].to_s
+      long = lat_long[:results].first[:locations].first[:latLng][:lng].to_s
+      distance = lat + ',' + long
       response = conn.get('/breweries') do |req|
         req.params['by_dist'] = distance
         req.params['per_page'] = quantity
