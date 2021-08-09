1fd0ecfdb7c68116a6809614276d9500971be9e2
Mark Yen <77414433+markcyen@users.noreply.github.com>
2021-08-09 10:20:42 -0600

    Add breweries route

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
       resources :forecast, only: :index
       resources :background, only: :index
       resources :users, only: :create
+      resources :breweries, only: :index
     end
   end
 end
