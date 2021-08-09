343f266e738583653610f435ecfef85d49d8900c
Mark Yen <77414433+markcyen@users.noreply.github.com>
2021-08-09 10:40:09 -0600

    Create brewery serializer

---
+++ app/serializers/brewery_serializer.rb
@@ -0,0 +1,20 @@
+class BrewerySerializer
+  include JSONAPI::Serializer
+
+  def self.send_breweries(data, location, current_weather)
+    {
+      data: {
+        id: nil,
+        type: 'breweries',
+        attributes: {
+          destination: location,
+          forecast: {
+            summary: current_weather[:conditions],
+            temperature: current_weather[:temperature].round(0).to_s + ' F'
+          },
+          breweries: data
+        }
+      }
+    }
+  end
+end
