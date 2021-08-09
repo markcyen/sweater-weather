da39caf496a620fdab76019f135bbb2c6bfee245
Mark Yen <77414433+markcyen@users.noreply.github.com>
2021-08-09 16:04:43 -0600

    Create tests sad paths

--- spec/requests/api/v1/user_requests_spec.rb
+++ spec/requests/api/v1/user_requests_spec.rb
@@ -33,5 +33,74 @@ RSpec.describe 'User Registration' do
       expect(response_body[:data][:attributes][:email]).to eq('username@test.com')
       expect(response_body[:data][:attributes]).to have_key :api_key
     end
+
+    it 'returns error when given an incorrect password' do
+      user_registration = {
+        email: 'userName@test.com',
+        password: 'password1',
+        password_confirmation: 'password'
+      }
+      headers = { 'CONTENT_TYPE' => 'application/json' }
+      post '/api/v1/users', headers: headers, params: JSON.generate(user: user_registration)
+
+      expect(response.status).to eq(400)
+
+      response_body = JSON.parse(response.body, symbolize_names: true)
+
+      expect(response_body[:data][:type]).to eq('error')
+      expect(response_body[:data][:error_message]).to eq("Password confirmation doesn't match Password")
+    end
+
+    it 'returns error when user email already exists' do
+      create(:user, email: 'username@test.com')
+      user_registration = {
+        email: 'username@test.com',
+        password: 'password1',
+        password_confirmation: 'password1'
+      }
+      headers = { 'CONTENT_TYPE' => 'application/json' }
+      post '/api/v1/users', headers: headers, params: JSON.generate(user: user_registration)
+
+      expect(response.status).to eq(400)
+
+      response_body = JSON.parse(response.body, symbolize_names: true)
+
+      expect(response_body[:data][:type]).to eq('error')
+      expect(response_body[:data][:error_message]).to eq('Email has already been taken')
+    end
+
+    it 'returns error when email is left blank' do
+      user_registration = {
+        email: '',
+        password: 'password1',
+        password_confirmation: 'password1'
+      }
+      headers = { 'CONTENT_TYPE' => 'application/json' }
+      post '/api/v1/users', headers: headers, params: JSON.generate(user: user_registration)
+
+      expect(response.status).to eq(400)
+
+      response_body = JSON.parse(response.body, symbolize_names: true)
+
+      expect(response_body[:data][:type]).to eq('error')
+      expect(response_body[:data][:error_message]).to eq("Email can't be blank and Email is invalid")
+    end
+
+    it 'returns error when email is invalid' do
+      user_registration = {
+        email: 'sdfjlsdjkfsdlk',
+        password: 'password1',
+        password_confirmation: 'password1'
+      }
+      headers = { 'CONTENT_TYPE' => 'application/json' }
+      post '/api/v1/users', headers: headers, params: JSON.generate(user: user_registration)
+
+      expect(response.status).to eq(400)
+
+      response_body = JSON.parse(response.body, symbolize_names: true)
+
+      expect(response_body[:data][:type]).to eq('error')
+      expect(response_body[:data][:error_message]).to eq("Email is invalid")
+    end
   end
 end
