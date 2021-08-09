03bce227679af252ae7048afbf723da8d49cbc23
Mark Yen <77414433+markcyen@users.noreply.github.com>
2021-08-09 11:13:11 -0600

    Implement conditionals for sad paths

--- app/controllers/api/v1/breweries_controller.rb
+++ app/controllers/api/v1/breweries_controller.rb
@@ -1,7 +1,12 @@
 class Api::V1::BreweriesController < ApplicationController
   def index
-    breweries = BreweriesFacade.get_breweries_data(params[:location], params[:quantity])
-    weather = WeatherFacade.get_weather_data(params[:location])
-    render json: BrewerySerializer.send_breweries(breweries, params[:location], weather.current_weather)
+    # binding.pry
+    if params[:quantity].to_i <= 0 || !params[:location].present? || numeric(params[:location])
+      render json: { error: 'Invalid query' }, status: :not_found
+    else
+      breweries = BreweriesFacade.get_breweries_data(params[:location], params[:quantity])
+      weather = WeatherFacade.get_weather_data(params[:location])
+      render json: BrewerySerializer.send_breweries(breweries, params[:location], weather.current_weather)
+    end
   end
 end
