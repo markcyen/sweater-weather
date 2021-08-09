0d9934398f7c48c3230408186b8f009ecbbeae50
Mark Yen <77414433+markcyen@users.noreply.github.com>
2021-08-09 10:52:47 -0600

    Refactor happy path test

--- spec/requests/api/v1/breweries_reqeusts_spec.rb
+++ spec/requests/api/v1/breweries_reqeusts_spec.rb
@@ -14,14 +14,46 @@ RSpec.describe 'Breweries Details API', :vcr do
         expect(json_output).to have_key :data
         expect(json_output[:data]).to be_a Hash
         expect(json_output[:data]).to have_key :id
+        expect(json_output[:data][:id]).to eq(nil)
         expect(json_output[:data]).to have_key :type
+        expect(json_output[:data][:type]).to eq('breweries')
         expect(json_output[:data]).to have_key :attributes
 
         expect(json_output[:data][:attributes]).to be_a Hash
         expect(json_output[:data][:attributes]).to have_key :destination
+        expect(json_output[:data][:attributes][:destination]).to eq('brighton, co')
         expect(json_output[:data][:attributes]).to have_key :forecast
+        expect(json_output[:data][:attributes][:forecast]).to be_a Hash
+        expect(json_output[:data][:attributes][:forecast]).to have_key :summary
+        expect(json_output[:data][:attributes][:forecast][:summary]).to eq('few clouds')
+        expect(json_output[:data][:attributes][:forecast]).to have_key :temperature
+        expect(json_output[:data][:attributes][:forecast][:temperature]).to eq('88 F')
         expect(json_output[:data][:attributes]).to have_key :breweries
+        expect(json_output[:data][:attributes][:breweries]).to be_an Array
+        expect(json_output[:data][:attributes][:breweries].size).to eq(5)
+
+        breweries_list = json_output[:data][:attributes][:breweries]
+
+        breweries_list.each do |brewery|
+          expect(brewery).to be_a Hash
+          expect(brewery).to have_key :id
+          expect(brewery).to have_key :name
+          expect(brewery).to have_key :brewery_type
+        end
+        expect(breweries_list.first[:id]).to eq(8862)
+        expect(breweries_list.first[:name]).to eq('Big Choice Brewing Co')
+        expect(breweries_list.first[:brewery_type]).to eq('micro')
+
+        expect(breweries_list.third[:id]).to eq(8762)
+        expect(breweries_list.third[:name]).to eq('Beer By Design Brewery')
+        expect(breweries_list.third[:brewery_type]).to eq('contract')
+
+        expect(breweries_list.last[:id]).to eq(13405)
+        expect(breweries_list.last[:name]).to eq('Periodic Brewing Northglenn')
+        expect(breweries_list.last[:brewery_type]).to eq('micro')
       end
+
+      
     end
   end
 end
