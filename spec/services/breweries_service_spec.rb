1ae036b232c3ecec47327179314236cf5dc61cea
Mark Yen <77414433+markcyen@users.noreply.github.com>
2021-08-09 09:33:39 -0600

    Create test to retrieve breweries API

---
+++ spec/services/breweries_service_spec.rb
@@ -0,0 +1,20 @@
+require 'rails_helper'
+
+RSpec.describe BreweriesService, :vcr do
+  describe 'class method' do
+    VCR.use_cassette('breweries service', :record => :new_episodes) do
+      describe '::retrieve_breweries' do
+        it 'can retrieve breweries' do
+          breweries_api = BreweriesService.retrieve_breweries('39.986767,-104.812604', 5)
+
+          expect(breweries_api).to be_a Array
+          expect(breweries_api.size).to eq(5)
+          expect(breweries_api.first).to be_a Hash
+          expect(breweries_api.first).to have_key :id
+          expect(breweries_api.first).to have_key :name
+          expect(breweries_api.first).to have_key :brewery_type
+        end
+      end
+    end
+  end
+end
