31870f88fb3d86c3f1166153fe311bb780c48ba0
Mark Yen <77414433+markcyen@users.noreply.github.com>
2021-08-09 16:05:55 -0600

    Add email format

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
   validates :email, :password_digest, presence: true
-  validates :email, uniqueness: true
+  validates :email, uniqueness: true, format: { with: URI::MailTo::EMAIL_REGEXP }
 
   has_secure_password
 end
