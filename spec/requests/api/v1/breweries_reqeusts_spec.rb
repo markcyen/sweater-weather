233dadaf9a7311aaaa8c50e9de4cd578b9b436a7
Mark Yen <77414433+markcyen@users.noreply.github.com>
2021-08-09 10:39:12 -0600

    Create breweries request test

--- spec/requests/api/v1/breweries_reqeusts_spec.rb
+++ spec/requests/api/v1/breweries_reqeusts_spec.rb
@@ -11,6 +11,16 @@ RSpec.describe 'Breweries Details API', :vcr do
         json_output = JSON.parse(response.body, symbolize_names: true)
 
         expect(json_output).to be_a Hash
+        expect(json_output).to have_key :data
+        expect(json_output[:data]).to be_a Hash
+        expect(json_output[:data]).to have_key :id
+        expect(json_output[:data]).to have_key :type
+        expect(json_output[:data]).to have_key :attributes
+
+        expect(json_output[:data][:attributes]).to be_a Hash
+        expect(json_output[:data][:attributes]).to have_key :destination
+        expect(json_output[:data][:attributes]).to have_key :forecast
+        expect(json_output[:data][:attributes]).to have_key :breweries
       end
     end
   end
