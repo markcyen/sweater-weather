d29cf95336a888ed8bfed34d1678f7ba4416374c
Mark Yen <77414433+markcyen@users.noreply.github.com>
2021-08-10 09:46:41 -0600

    Implement retrieve directions class method

--- app/services/geocode_service.rb
+++ app/services/geocode_service.rb
@@ -1,5 +1,14 @@
 class GeocodeService
   class << self
+    def retrieve_directions(origin, destination)
+      response = conn.get('/directions/v2/route') do |req|
+        req.params['key'] = ENV['geocode_key']
+        req.params['from'] = origin
+        req.params['to'] = destination
+      end
+      parse_json(response)
+    end
+
     def retrieve_geocode(city_state)
       response = conn.get('/geocoding/v1/address') do |req|
         req.params['key'] = ENV['geocode_key']
