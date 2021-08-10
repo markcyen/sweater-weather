27ebe9b66432e035f0340c7508f0afef5974df5a
Mark Yen <77414433+markcyen@users.noreply.github.com>
2021-08-10 15:11:56 -0600

    Satisfy happy path conditionals

--- app/serializers/road_trip_serializer.rb
+++ app/serializers/road_trip_serializer.rb
@@ -9,16 +9,25 @@ class RoadTripSerializer
         attributes: {
           start_city: origin,
           end_city: destination,
-          travel_time: "#{trip_data.formatted_time.split(':')[0]} hours, #{trip_data.formatted_time.split(':')[1]} minutes",
+          travel_time:
+            if trip_data.formatted_time.nil?
+              'impossible'
+            else
+              "#{trip_data.formatted_time.split(':')[0]} hours, #{trip_data.formatted_time.split(':')[1]} minutes"
+            end,
           weather_at_eta: {
             temperature:
-              if trip_data.formatted_time.split(':')[0].to_i > 47
+              if trip_data.formatted_time.nil?
+                ''
+              elsif trip_data.formatted_time.split(':')[0].to_i > 47
                 weather_at_eta.hourly_weather[47][:temperature]
               else
                 weather_at_eta.hourly_weather[trip_data.formatted_time.split(':')[0].to_i][:temperature]
               end,
             conditions:
-              if trip_data.formatted_time.split(':')[0].to_i > 47
+              if trip_data.formatted_time.nil?
+                ''
+              elsif trip_data.formatted_time.split(':')[0].to_i > 47
                 weather_at_eta.hourly_weather[47][:conditions]
               else
                 weather_at_eta.hourly_weather[trip_data.formatted_time.split(':')[0].to_i][:conditions]
